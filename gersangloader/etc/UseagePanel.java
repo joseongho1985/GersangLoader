31c9555e6c4b75e77b3d5df8b5848a4fd033eb87
joseongho1985 <joseongho1985@gmail.com>
2025-11-14 11:21:03 +0900

    Update UseagePanel.java

--- gersangloader/etc/UseagePanel.java
+++ gersangloader/etc/UseagePanel.java
@@ -14,20 +14,23 @@ public class UseagePanel extends JPanel {
 	 * Create the panel.
 	 */
 	public UseagePanel() {
-		setLayout(new GridLayout(4, 1, 0, 0));
+		setLayout(new GridLayout(5, 1, 0, 0));
 		
 		JLabel lblNewLabel = new JLabel("사용방법");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("1. 설정 > 클라이언트 에서 클라이언트를 선택한다.");
+		JLabel lblNewLabel_1 = new JLabel("1. 설정 > 클라이언트에서 클라이언트를 선택해주세요.");
 		add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("2. 설정 > 로그인에서 아이디와 비밀번호를 입력한다.");
+		JLabel lblNewLabel_2 = new JLabel("2. 설정 > 로그인에서 아이디와 비밀번호를 입력해주세요.");
 		add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("3. 로그인 단추를 누른후, 실행 버튼을 누른다.");
+		JLabel lblNewLabel_3 = new JLabel("3. 로그인 단추를 눌러주세요.");
 		add(lblNewLabel_3);
+
+		JLabel lblNewLabel_4 = new JLabel("4. 실행 버튼을 눌러주세요.");
+		add(lblNewLabel_4);
 
 	}
 
